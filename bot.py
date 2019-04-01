2979c5f6c111a4c9c51ec0ae6c4ec497f8071e3b
Egor Suvorov <egor.suvorov@gmail.com>
2019-04-01 13:20:23 +0300

    bot.py: add comments

--- bot.py
+++ bot.py
@@ -6,21 +6,22 @@ def send_money(src_key, dest_key, amount):
     dest_id = app.USER_ID_TO_SMALL_ID[dest_key]
     src_name = app.USER_ID_TO_NAME[src_key]
     dest_name = app.USER_ID_TO_NAME[dest_key]
+	# ^^^^^^^^^ наверху технический код, скука ^^^^^^^^^
 
-    app.send_msg(src_key,
+    app.send_msg(src_key,  # Сообщение отправителю
         f'Получен запрос на перевод в {amount} монет.\n'
         f'От кого: {src_id} ({src_name})\n'
         f'Кому: {dest_id} ({dest_name})\n')
 
-    yield
+    yield  # Задержка 5 секунд
 
-    app.USER_ID_TO_MONEY[src_key] -= amount
-    app.USER_ID_TO_MONEY[dest_key] += amount
+    app.USER_ID_TO_MONEY[src_key] -= amount  # Уменьшить баланс отправителя
+    app.USER_ID_TO_MONEY[dest_key] += amount  # Увеличить баланс получателя
 
-    app.send_msg(src_key,
+    app.send_msg(src_key,  # Сообщение отправителю
         f'Перевод выполнен!\n'
         f'Ваш баланс: {app.USER_ID_TO_MONEY[src_key]}')
-    app.send_msg(dest_key,
+    app.send_msg(dest_key,  # Сообщение получателю
         f'Вы получили {amount} монет.\n'
         f'От кого: {src_id} {src_name}\n'
         f'Ваш баланс: {app.USER_ID_TO_MONEY[dest_key]}')
